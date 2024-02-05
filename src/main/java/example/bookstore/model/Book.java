cf52fee51ca01a9ec05690ddd203faca374dc563
sarahonk <honkanensara@gmail.com>
2024-02-05 13:35:09 +0200

    first commit

---
+++ src/main/java/example/bookstore/model/Book.java
@@ -0,0 +1,60 @@
+package example.bookstore.model;
+
+public class Book {
+
+    private long id;
+    private String title;
+    private String author;
+    private int publicationyear;
+    private String isbn;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPublicationyear() {
+        return publicationyear;
+    }
+
+    public void setPublicationyear(int publicationyear) {
+        this.publicationyear = publicationyear;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    private double price;
+}
