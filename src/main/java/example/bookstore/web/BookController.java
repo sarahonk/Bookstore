d4be4318f64a7b86cfd9f9aa0b7bb26f6847f8c5
sarahonk <honkanensara@gmail.com>
2024-02-26 16:23:25 +0200

    Lisäyksiä jälleen C:

--- src/main/java/example/bookstore/web/BookController.java
+++ src/main/java/example/bookstore/web/BookController.java
@@ -4,7 +4,7 @@ import example.bookstore.model.Book;
 import example.bookstore.repository.BookRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +22,23 @@ public class BookController {
         List<Book> books = bookRepository.findAll();
         model.addAttribute("books", books);
         return "booklist";
+    }
+
+    @GetMapping("/addbook")
+    public String showAddBookForm(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @PostMapping("/saveBook")
+    public String saveBook(@ModelAttribute Book book) {
+        bookRepository.save(book);
+        return "redirect:/booklist";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") long id) {
+        bookRepository.deleteById(id);
+        return "redirect:/booklist";
     }
 }
